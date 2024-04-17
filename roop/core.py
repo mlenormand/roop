bb7544a9c62a9408e86d793b7084a2cb9cc8e781
Mickaël Le Normand <mickael.lenormand@thetribe.io>
2024-04-17 22:19:17 +0200

    wip

--- roop/core.py
+++ roop/core.py
@@ -57,7 +57,7 @@ def parse_args() -> None:
     roop.globals.target_path = args.target_path
     roop.globals.faces_path = args.faces_path
     roop.globals.output_path = normalize_output_path(args.source_folder, roop.globals.target_path, args.output_path)
-    roop.globals.headless = roop.globals.source_folder is not None and roop.globals.target_path is not None and roop.globals.output_path is not None
+    roop.globals.headless = args.source_folder is not None and roop.globals.target_path is not None and roop.globals.output_path is not None
     roop.globals.frame_processors = args.frame_processor
     roop.globals.keep_fps = args.keep_fps
     roop.globals.keep_frames = args.keep_frames
